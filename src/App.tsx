079fcce9c6dcba52055239b4ef9323268259a79a
rachit0malik-bota <rachit0malik@gmail.com>
2026-04-14 10:36:27 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+/**
+ * @license
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import { ThemeProvider } from './components/theme-provider';
+import { Layout } from './components/layout/Layout';
+import { Home } from './pages/Home';
+import { About } from './pages/About';
+import { Services } from './pages/Services';
+import { Portfolio } from './pages/Portfolio';
+import { Contact } from './pages/Contact';
+
+export default function App() {
+  return (
+    <ThemeProvider defaultTheme="system" storageKey="theme">
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="services" element={<Services />} />
+            <Route path="portfolio" element={<Portfolio />} />
+            <Route path="contact" element={<Contact />} />
+          </Route>
+        </Routes>
+      </HashRouter>
+    </ThemeProvider>
+  );
+}
